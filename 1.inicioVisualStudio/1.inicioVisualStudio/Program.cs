28d17879c1641b9646b04dbfa0e474e33fe502fc
Samuel Restrepo <samuelrestrepomejia@gmail.com>
2025-07-25 15:01:20 -0500

    primer commit

---
+++ 1.inicioVisualStudio/1.inicioVisualStudio/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1.inicioVisualStudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
