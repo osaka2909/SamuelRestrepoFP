22a03da230c8ff6526bbfa52879b24bee3407882
osaka2909 <samuelrestrepomejia@gmail.com>
2025-10-17 14:14:27 -0500

    ciclo while

---
+++ 9-ciclowhile/9-ciclowhile/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _9_ciclowhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contador = 1;
+            int acumulador= 0;
+
+            while (contador <= 5) 
+            {
+                acumulador += contador; //acumulador = acumulador + contador
+                contador++;
+            }
+
+            Console.WriteLine($"la suma de los cinco primeros numeros enteros es: {acumulador}");
+        }
+    }
+}
